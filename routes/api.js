fb976077848885b7c49592345a85a77211f54925
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2021-04-27 19:15:56 +0700

    Add database and some endpoints

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,28 @@ var db = require('../database');
 
 router.get('/problem/:problemId');
 
-router.get('/problems', function(req, res, next) {
-  db.find({}, function(err, docs) {
+router.get('/problems', function (req, res, next) {
+  db.find({}, function (err, docs) {
     res.json(docs);
   });
 });
 
-router.get('/newProblem', function(req, res, next) {
-  db.getNextId(function(err, docs) {
-    console.log('a')
-    db.insert({_id: docs, name:"test"}, function(err, docs) {
-      res.send(docs);
+router.post('/newProblem', function (req, res, next) {
+  body = req.body;
+  if (body.question === undefined) {
+    res.status(400).send('Invalid Question');
+  } else if (body.answers === undefined || !Array.isArray(body.answers)) {
+    res.status(400).send('Invalid Answers');
+  } else {
+    docs = {
+      question: req.body.question,
+      answers: req.body.answers
+    };
+
+    db.insertSequential(docs, function (err, docs) {
+      res.json(docs);
     });
-  })
+  }
 });
-
-
 
 module.exports = router;
