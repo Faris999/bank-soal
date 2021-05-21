5f4c06272f5fc7dd03dd081bd72711c5d5f33012
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2021-05-21 08:41:04 +0700

    Fix build settings 3

--- buildClient.js
+++ buildClient.js
@@ -1,5 +1,5 @@
 const args1 = [ 'install' ];
 const args2 = [ 'run', 'build' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args1, opts);
-require('child_process').spawn('npm', args2, opts);
+require('child_process').spawnSync('npm', args1, opts);
+require('child_process').spawnSync('npm', args2, opts);
